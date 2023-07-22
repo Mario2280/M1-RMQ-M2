ba6932815c980c1111a3db624fb779342aca1e8b
David Gulkevich <david2017ra@gmail.com>
2023-07-22 15:55:07 +0300

    test 1 space

--- m1.ts
+++ m1.ts
@@ -1,47 +1,44 @@
+import bodyParser from 'body-parser'
+import { Channel, connect } from 'amqplib'
+import express, { Response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
-import bodyParser from 'body-parser';
-import { Channel, connect } from 'amqplib';
-import express, { Response } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+const PORT = 3001
+const RABBITMQ_URL = 'amqp://localhost'
+const EXCHANGE_NAME = 'taskExchange'
+const QUEUE_NAME = 'taskQueue'
+const OPERATIONS = ['plus', 'munis', 'div', 'mult']
 
-
-
-const PORT = 3001;
-const RABBITMQ_URL = 'amqp://localhost' 
-const EXCHANGE_NAME = 'taskExchange';
-const QUEUE_NAME = 'taskQueue';
-const OPERATIONS = ['plus', 'munis', 'div', 'mult'];
-
-
-
-let channel: Channel, connection;
-const app = express();
-app.use(bodyParser.json());
-
+let channel: Channel, connection
+const app = express()
+app.use(bodyParser.json())
 
 async function createRMQConnection() {
-  try {
-    connection = await connect(RABBITMQ_URL);
-    channel = await connection.createChannel();
+	try {
+		connection = await connect(RABBITMQ_URL)
+		channel = await connection.createChannel()
 		channel.assertExchange(EXCHANGE_NAME, 'direct', { durable: true })
-  } catch(e){
-    console.error(e);
-  }
+	} catch (e) {
+		console.error(e)
+	}
 }
 
+;(async () => {
+	await createRMQConnection()
 
-(async () => {
-
-	await createRMQConnection();
-
-  app.post('/runTask', async (req, res: Response) => {
+	app.post('/runTask', async (req, res: Response) => {
 		try {
-			
 			const operation = req.body.operation
 			const { numbers } = req.body
-			if (!numbers || !Array.isArray(numbers) || numbers.length < 2 || !OPERATIONS.includes(operation) || numbers[1] == 0) {
+			if (
+				!numbers ||
+				!Array.isArray(numbers) ||
+				numbers.length < 2 ||
+				!OPERATIONS.includes(operation) ||
+				numbers[1] == 0
+			) {
 				res.status(400).json({ error: 'Invalid input' })
-				return;
+				return
 			}
 
 			const correlationId = uuidv4()
@@ -57,7 +54,7 @@ async function createRMQConnection() {
 						}
 					},
 					{ noAck: true }
-				);
+				)
 				channel.publish(
 					EXCHANGE_NAME,
 					'calculator.command',
@@ -69,7 +66,7 @@ async function createRMQConnection() {
 				)
 			})
 
-			const result = await requestTask;
+			const result = await requestTask
 
 			res.status(200).json({ result })
 		} catch (error) {
@@ -81,6 +78,4 @@ async function createRMQConnection() {
 	app.listen(PORT, () => {
 		console.log(`Server is running on port ${PORT}`)
 	})
-
-
-})();
+})()
--- m2.ts
+++ m2.ts
@@ -2,46 +2,49 @@
 import { connect } from 'amqplib'
 
 const RABBITMQ_URL = 'amqp://localhost'
-const EXCHANGE_NAME = 'taskExchange';
-const QUEUE_NAME = 'taskQueue';
-
+const EXCHANGE_NAME = 'taskExchange'
+const QUEUE_NAME = 'taskQueue'
 
 const run = async () => {
-  try {
-    const connection = await connect(RABBITMQ_URL)
-    const channel = await connection.createChannel();
-    await channel.assertExchange(EXCHANGE_NAME, 'direct', {durable: true});
-    const queue = await channel.assertQueue(QUEUE_NAME, { durable: true })
-    channel.bindQueue(QUEUE_NAME, EXCHANGE_NAME, 'calculator.command')
-    channel.consume(queue.queue, msg => {
-			if (!msg) return
+	try {
+		const connection = await connect(RABBITMQ_URL)
+		const channel = await connection.createChannel()
+		await channel.assertExchange(EXCHANGE_NAME, 'direct', { durable: true })
+		const queue = await channel.assertQueue(QUEUE_NAME, { durable: true })
+		channel.bindQueue(QUEUE_NAME, EXCHANGE_NAME, 'calculator.command')
+		channel.consume(
+			queue.queue,
+			(msg) => {
+				if (!msg) return
 
-      const { operation, numbers } = JSON.parse(msg.content.toString())
-      let res;
-      switch (operation) {
-        case 'plus':
-          res = numbers[0] + numbers[1]
-          break
-        case 'munis':
-          res = numbers[0] - numbers[1]
-          break
-        case 'div':
-          res = numbers[0] / numbers[1]
-          break
-        case 'mult':
-          res = numbers[0] * numbers[1];
-          break
-      }
+				const { operation, numbers } = JSON.parse(msg.content.toString())
+				let res
+				switch (operation) {
+					case 'plus':
+						res = numbers[0] + numbers[1]
+						break
+					case 'munis':
+						res = numbers[0] - numbers[1]
+						break
+					case 'div':
+						res = numbers[0] / numbers[1]
+						break
+					case 'mult':
+						res = numbers[0] * numbers[1]
+						break
+				}
 
-			channel.sendToQueue(msg.properties.replyTo, Buffer.from(res.toString()), {
-				correlationId: msg.properties.correlationId,
-			})
-		}, {
-      noAck: true
-    })
-  } catch (error) {
-    console.log(error);
-  }
+				channel.sendToQueue(msg.properties.replyTo, Buffer.from(res.toString()), {
+					correlationId: msg.properties.correlationId,
+				})
+			},
+			{
+				noAck: true,
+			}
+		)
+	} catch (error) {
+		console.log(error)
+	}
 }
 
-run();
+run()
